c8f13d72ffa8ff1ee780f7b3ab9ccef9e5d70520
Zahra <zahra@Zahras-Mac.local>
2019-01-14 13:06:09 -0800

    Initial Commit

---
+++ ibeacon/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ibeacon
+//
+//  Created by Zahra on 1/14/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
